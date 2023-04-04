6b6b49f6edfea4436023d1b78b5d37e342ac60d6
jisubaek <bbbjisu@gmail.com>
2023-04-04 16:16:26 +0900

    model에 category 추가함

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -3,6 +3,17 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200,unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
@@ -14,9 +25,11 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     author = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f'[{self.pk}]{self.title}'
+
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
